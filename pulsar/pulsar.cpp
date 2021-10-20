d825a296fead4b0ec5b975d369b8ea48b1a10f9c
Herb Miller Jr <herb@hlmjr.com>
2021-10-20 15:22:40 -0400

    Implement switching scenes via plugin

--- pulsar/pulsar.cpp
+++ pulsar/pulsar.cpp
@@ -1,6 +1,8 @@
 #include <obs-module.h>
 #include <obs-frontend-api.h>
+#include <QLocalServer>
 #include <QLocalSocket>
+#include <unordered_map>
 #include <iostream>
 
 OBS_DECLARE_MODULE()
@@ -15,9 +17,72 @@ void HandleEvent(obs_frontend_event event,void *data)
 	}
 }
 
-bool obs_module_load(void)
+enum Triggers
+{
+	SWITCH_SCENE,
+	ENABLE_SOURCE,
+	DISABLE_SOURCE
+};
+
+QLocalServer *server;
+std::unordered_map<QByteArray,int> triggers;
+
+bool obs_module_load()
 {
 	obs_frontend_add_event_callback(HandleEvent,nullptr);
 
+	triggers.insert({"switch_scene",SWITCH_SCENE});
+	triggers.insert({"enable_source",ENABLE_SOURCE});
+	triggers.insert({"disable_source",DISABLE_SOURCE});
+
+	server=new QLocalServer();
+	server->connect(server,&QLocalServer::newConnection,[]() {
+		std::cout << "Connection established!" << std::endl;
+		QLocalSocket *socket=server->nextPendingConnection();
+		socket->connect(socket,&QLocalSocket::readyRead,[socket]() {
+			QList<QByteArray> data=socket->readAll().trimmed().split('|');
+			QByteArray command=data.front();
+			QByteArray message=data.back();
+			std::cout << "Data received: " << command.toStdString() << std::endl;
+			if (triggers.find(command) == triggers.end()) return;
+			switch (triggers.at(command))
+			{
+			case SWITCH_SCENE:
+			{
+				std::cout << "Switch Scene" << std::endl;
+				struct obs_frontend_source_list scenes={0};
+				obs_frontend_get_scenes(&scenes);
+				for (size_t scene=0; scene < scenes.sources.num; scene++)
+				{
+					obs_source_t *details=scenes.sources.array[scene];
+					const char *name=obs_source_get_name(details);
+					std::cout << "Testing name " << name << " vs. " << message.data() << std::endl;
+					if (QString(name) == QString(message))
+					{
+						std::cout << "Setting scene...";
+						obs_frontend_set_current_scene(details);
+					}
+				}
+
+				break;
+			}
+			case ENABLE_SOURCE:
+				std::cout << "Enable Source" << std::endl;
+				break;
+			case DISABLE_SOURCE:
+				std::cout << "Disable Source" << std::endl;
+				break;
+			}
+		});
+	});
+	if (!server->listen("pulsar-obs")) std::cout << "Failed to creatte Pulsar server." << std::endl;
+
+	std::cout << "Pulsar loaded!" << std::endl;
+
 	return true;
+}
+
+void obs_module_unload()
+{
+	delete server;
 }
