b80052cb944da51a3831409779d1a57d80627204
Herb Miller Jr <herb@hlmjr.com>
2022-10-13 17:00:00 -0400

    Official application icon

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main(int argc,char *argv[])
 	QApplication application(argc,argv);
 	application.setOrganizationName(ORGANIZATION_NAME);
 	application.setApplicationName(APPLICATION_NAME);
-	application.setWindowIcon(QIcon(":/celeste_256.png"));
+	if constexpr (Platform::Windows()) application.setWindowIcon(QIcon(":/celeste.png"));
 
 #ifdef DEVELOPER_MODE
 	if (QMessageBox(QMessageBox::Warning,"DEVELOPER MODE","**WARNING** Celeste is currently in developer mode. Sensitive data will be displayed in the main window and written to the log. Only proceed if you know what you are doing. Continue?",QMessageBox::Yes|QMessageBox::No).exec() == QMessageBox::No) return OK;
